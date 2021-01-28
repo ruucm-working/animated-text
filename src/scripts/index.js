68fe953ccddc903f13bf6830adbffb2e353c55a5
ruucm <ruucm.a@gmail.com>
2021-01-28 21:49:54 +0900

    rm unusings

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,16 +23,16 @@ engine.start()
 const tlShow = new TimelineLite({ delay: 0.2, onStart: () => {
   // lineGenerator.start();
 }});
-tlShow.to('.overlay', 2, { opacity: 0 });
-tlShow.to('.background', 2, { y: -300 }, 0);
+// tlShow.to('.overlay', 2, { opacity: 0 });
+// tlShow.to('.background', 2, { y: -300 }, 0);
 tlShow.fromTo(engine.lookAt, 2, { y: -8 }, { y: 0 }, 0);
 tlShow.add(text.show, '-=1');
 
 // Hide
-app.onHide((onComplete) => {
-  const tlHide = new TimelineLite();
-  tlHide.to(engine.lookAt, 2, { y: -6, ease: Power3.easeInOut });
-  tlHide.add(text.hide, 0);
-  tlHide.add(lineGenerator.stop);
-  tlHide.to('.overlay', 0.5, { autoAlpha: 1, onComplete }, '-=1.5');
-});
+// app.onHide((onComplete) => {
+//   const tlHide = new TimelineLite();
+//   tlHide.to(engine.lookAt, 2, { y: -6, ease: Power3.easeInOut });
+//   tlHide.add(text.hide, 0);
+//   tlHide.add(lineGenerator.stop);
+//   tlHide.to('.overlay', 0.5, { autoAlpha: 1, onComplete }, '-=1.5');
+// });
